f7affc6296c001fce5d6c0da7428326e81932cb7
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-01-15 23:12:04 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>あすか</h1>
+      <p>nagai</p>
+    </>
+  );
+};
+
+//他のファイルで関数を使うために
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+//ライブラリからインポート
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+//関数を使うrender(1関数名、2場所)
+ReactDom.render(<App />, document.getElementById("root"));
