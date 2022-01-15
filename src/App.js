9488cb584aad6d7af02be53b002efb06d4d984ec
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-01-15 23:30:14 +0900

    1day

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
+  //関数でstyleをあてる
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
   return (
     <>
-      <h1>あすか</h1>
-      <p>nagai</p>
+      <h1 style={{ color: "red" }}>あすか</h1>
+      <p style={contentStyle}>nagai</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
